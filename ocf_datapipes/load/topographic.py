80a2451e08ebb0a016f6f986963912f0d41ae299
Jacob Bieker <jacob@bieker.tech>
2022-08-08 16:05:15 +0100

    Add opening Topographic data

---
+++ ocf_datapipes/load/topographic.py
@@ -0,0 +1,23 @@
+from torchdata.datapipes.iter import IterDataPipe
+from torchdata.datapipes import functional_datapipe
+from typing import Union
+from pathlib import Path
+import rioxarray
+
+@functional_datapipe("open_topography")
+class OpenTopographyIterDataPipe(IterDataPipe):
+    def __init__(self, topo_filename: Union[Path, str]):
+        self.topo_filename = topo_filename
+
+    def __iter__(self):
+        topo = rioxarray.open_rasterio(filename=self.topo_filename, parse_coordinates=True, masked=True)
+
+        # `band` and `spatial_ref` don't appear to hold any useful info. So get rid of them:
+        topo = topo.isel(band=0)
+        topo = topo.drop_vars(["spatial_ref", "band"])
+
+        # Use our standard naming:
+        topo = topo.rename({"x": "x_osgb", "y": "y_osgb"})
+
+        while True:
+            yield topo
