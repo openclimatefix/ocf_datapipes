da252ea0575de6b91167b7cf8a3b071fc4de8c89
Jacob Bieker <jacob@bieker.tech>
2022-08-08 16:07:44 +0100

    Add import of topography

--- ocf_datapipes/load/__init__.py
+++ ocf_datapipes/load/__init__.py
@@ -3,3 +3,9 @@ from .pv import OpenPVFromNetCDFIterDataPipe as OpenPVFromNetCDF
 from .satellite import OpenSatelliteDataPipe as OpenSatellite
 from .nwp import OpenNWPIterDataPipe as OpenNWP
 from .gsp import OpenGSPIterDataPipe as OpenGSP
+try:
+    import rioxarray # Rioxarray is sometimes a pain to install, so only load this if its installed
+    from .topographic import OpenTopographyIterDataPipe as OpenTopography
+except ImportError:
+    print("Rioxarray is not installed, so not importing OpenTopography")
+    pass
