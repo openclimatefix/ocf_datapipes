bcd7e204b5f3839fae813288527f8c1f0c767fa3
Jacob Bieker <jacob@bieker.tech>
2022-08-11 14:02:24 +0100

    Add stub offset t0 datapipe

---
+++ ocf_datapipes/select/offset_t0.py
@@ -0,0 +1,14 @@
+from torchdata.datapipes.iter import IterDataPipe
+from torchdata.datapipes import functional_datapipe
+import numpy as np
+from typing import Union
+
+@functional_datapipe("offset_t0")
+class OffsetT0IterDataPipe(IterDataPipe):
+    def __init__(self, source_dp: IterDataPipe, max_t0_offset_minutes: Union[float, int], min_t0_offset_minutes: Union[float, int] = 0.0):
+        self.source_dp = source_dp
+        self.max_t0_offset_minutes = max_t0_offset_minutes
+        self.min_t0_offset_minutes = min_t0_offset_minutes
+
+    def __iter__(self):
+        pass
