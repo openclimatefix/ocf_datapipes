cdc8d98523ac1fbe0102c0dea0cbda660136b22c
Jacob Bieker <jacob@bieker.tech>
2022-08-11 15:00:08 +0100

    Add test imports

--- tests/end2end/test_power_perceiver_production.py
+++ tests/end2end/test_power_perceiver_production.py
@@ -1,1 +1,6 @@
 from ocf_datapipes.load import OpenTopography, OpenNWP, OpenGSP, OpenSatellite, OpenPVFromNetCDF
+from ocf_datapipes.transform.numpy import AlignGSPto5Min, AddSunPosition, AddTopographicData, SaveT0Time, EncodeSpaceTime
+from ocf_datapipes.transform.xarray import SelectPVSystemsWithinRegion, ReduceNumPVSystems
+
+def test_power_perceiver_production():
+    pass
