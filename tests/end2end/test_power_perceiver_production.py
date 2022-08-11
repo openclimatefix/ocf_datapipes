49e46ea61ff8aa3e833cc440ae8367967c851093
Jacob Bieker <jacob@bieker.tech>
2022-08-11 15:36:23 +0100

    Start on test

--- tests/end2end/test_power_perceiver_production.py
+++ tests/end2end/test_power_perceiver_production.py
@@ -1,6 +1,15 @@
 from ocf_datapipes.load import OpenTopography, OpenNWP, OpenGSP, OpenSatellite, OpenPVFromNetCDF
 from ocf_datapipes.transform.numpy import AlignGSPto5Min, AddSunPosition, AddTopographicData, SaveT0Time, EncodeSpaceTime
 from ocf_datapipes.transform.xarray import SelectPVSystemsWithinRegion, ReduceNumPVSystems
+import torchdata.datapipes as dp
 
 def test_power_perceiver_production():
+
+    sat_datapipe = OpenSatellite()
+    pv_datapipe = OpenPVFromNetCDF()
+    gsp_datapipe = OpenGSP()
+    nwp_datapipe = OpenNWP()
+    topo_datapipe = OpenTopography()
+
+
     pass
