a66ae62a896f61ec330b77d0d9d7502236256135
Jacob Bieker <jacob@bieker.tech>
2022-08-12 15:37:07 +0100

    Add downsampling

--- ocf_datapipes/transform/xarray/__init__.py
+++ ocf_datapipes/transform/xarray/__init__.py
@@ -2,4 +2,4 @@ from .convert_satellite_to_int import ConvertSatelliteToInt8IterDataPipe as Conv
 from .reduce_num_pv_systems import ReduceNumPVSystemsIterDataPipe as ReduceNumPVSystems
 
 from .add_contiguous_time_periods import AddContiguousT0TimePeriodsIterDataPipe as AddContiguousT0TimePeriods
-
+from .downsample import DownsampleIterDataPipe as Downsample
