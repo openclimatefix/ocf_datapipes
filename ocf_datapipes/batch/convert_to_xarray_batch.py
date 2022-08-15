f79bb735f52ac2cc8bb8e4fe1c53c503e931a792
Jacob Bieker <jacob@bieker.tech>
2022-08-15 13:01:35 +0100

    Add stub converting to XarrayBatch

--- ocf_datapipes/batch/convert_to_xarray_batch.py
+++ ocf_datapipes/batch/convert_to_xarray_batch.py
@@ -1,2 +1,17 @@
 from torchdata.datapipes import functional_datapipe
-from torchdata.datapipes.iter import IterDataPipe
+from torchdata.datapipes.iter import IterDataPipe, Zipper
+import xarray as xr
+from ocf_datapipes.utils import XarrayBatch
+from typing import Union, Iterable
+
+@functional_datapipe("convert_to_xarray_batch")
+class ConvertToXarrayBatchIterDataPipe(IterDataPipe):
+    def __init__(self, source_dps: Iterable[IterDataPipe]):
+        super().__init__()
+        self.source_dps = source_dps
+
+    def __iter__(self) -> XarrayBatch:
+        for xr_datas in Zipper(*self.source_dps):
+            xr_batch = XarrayBatch()
+            # TODO Combine here to batch
+            yield xr_batch
