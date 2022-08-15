b23f957474d89264748b451d47e3fa421922311d
Jacob Bieker <jacob@bieker.tech>
2022-08-15 13:03:51 +0100

    Add stub converting to NumpyBatch

--- ocf_datapipes/batch/convert_xarray_batch_to_numpy_batch.py
+++ ocf_datapipes/batch/convert_xarray_batch_to_numpy_batch.py
@@ -1,2 +1,16 @@
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
+import xarray as xr
+import numpy as np
+from ocf_datapipes.utils import NumpyBatch
+
+@functional_datapipe("convert_xarray_batch_to_numpy_batch")
+class ConvertXarrayBatchToNumpyBatch(IterDataPipe):
+    def __init__(self, source_dp: IterDataPipe):
+        self.source_dp = source_dp
+
+    def __iter__(self) -> NumpyBatch:
+        for xr_batch in self.source_dp:
+            # TODO Actually convert it, or add conversion to XarrayBatch
+            np_batch: NumpyBatch = xr_batch.to_numpy()
+            yield np_batch
