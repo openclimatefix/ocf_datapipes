f6c0e71abf630cd0cda4ac581fb69f7a75072b6a
Jacob Bieker <jacob@bieker.tech>
2022-08-17 18:26:04 +0100

    Fix paths

--- tests/load/test_load_gsp.py
+++ tests/load/test_load_gsp.py
@@ -1,4 +1,4 @@
 from ocf_datapipes.load import OpenGSP
 
 def test_open_gsp():
-    gsp_dp = OpenGSP("/home/jacob/Development/ocf_datapipes/tests/data/gsp/test.zarr")
+    gsp_dp = OpenGSP("tests/data/gsp/test.zarr")
--- tests/load/test_load_satellite.py
+++ tests/load/test_load_satellite.py
@@ -1,7 +1,7 @@
 from ocf_datapipes.load import OpenSatellite
 
 def test_open_satellite():
-    sat_dp = OpenSatellite(zarr_path="/home/jacob/Development/ocf_datapipes/tests/data/hrv_sat_data.zarr")
+    sat_dp = OpenSatellite(zarr_path="tests/data/hrv_sat_data.zarr")
     metadata = next(iter(sat_dp))
     assert metadata is not None
 
