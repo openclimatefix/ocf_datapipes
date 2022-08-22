ab7657b5986f184b4b209b334af6f3efcb75ed75
Jacob Bieker <jacob@bieker.tech>
2022-08-22 14:23:11 +0100

    Add empty tests for the rest of PP production parts

---
+++ tests/transform/numpy/test_sun_position.py
@@ -0,0 +1,16 @@
+from ocf_datapipes.transform.numpy import AddSunPosition
+
+def test_add_sun_position_hrvsatellite(sat_hrv_dp):
+    pass
+
+def test_add_sun_position_satellite(sat_dp):
+    pass
+
+def test_add_sun_position_gsp(gsp_dp):
+    pass
+
+def test_add_sun_position_passiv(passiv_dp):
+    pass
+
+def test_add_sun_position_nwp(nwp_dp):
+    pass
