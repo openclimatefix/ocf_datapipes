dfd4c798f3b5f08e5f03add44e77095b312ce3d5
Jacob Bieker <jacob@bieker.tech>
2022-08-23 19:06:18 +0100

    Iterate through all locations in a dataset

--- ocf_datapipes/select/location_picker.py
+++ ocf_datapipes/select/location_picker.py
@@ -5,14 +5,21 @@ from torchdata.datapipes.iter import IterDataPipe
 
 @functional_datapipe("location_picker")
 class LocationPickerIterDataPipe(IterDataPipe):
-    def __init__(self, source_dp: IterDataPipe):
+    def __init__(self, source_dp: IterDataPipe, return_all_locations: bool = False):
         super().__init__()
         self.source_dp = source_dp
+        self.return_all_locations = return_all_locations
 
     def __iter__(self):
         for xr_dataset in self.source_dp:
-            # Assumes all datasets have osgb coordinates for selecting locations
-            # Pick 1 random location from the input dataset
-            location_idx = np.random.randint(0, len(xr_dataset["x_osgb"]))
-            location = (xr_dataset["x_osgb"][location_idx], xr_dataset["y_osgb"][location_idx])
-            yield location
+            if self.return_all_locations:
+                # Iterate through all locations in dataset
+                for location_idx in range(len(xr_dataset["x_osgb"])):
+                    location = (xr_dataset["x_osgb"][location_idx], xr_dataset["y_osgb"][location_idx])
+                    yield location
+            else:
+                # Assumes all datasets have osgb coordinates for selecting locations
+                # Pick 1 random location from the input dataset
+                location_idx = np.random.randint(0, len(xr_dataset["x_osgb"]))
+                location = (xr_dataset["x_osgb"][location_idx], xr_dataset["y_osgb"][location_idx])
+                yield location
