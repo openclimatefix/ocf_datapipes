902de50e9ac98743bf503a0cdde8efeb8d517ff1
Jacob Bieker <jacob@bieker.tech>
2022-08-24 16:38:40 +0100

    Add notes

--- tests/transform/numpy/test_sun_position.py
+++ tests/transform/numpy/test_sun_position.py
@@ -1,8 +1,21 @@
 from ocf_datapipes.transform.numpy import AddSunPosition
+from ocf_datapipes.transform.numpy import EncodeSpaceTime, AlignGSPto5Min, SaveT0Time, ExtendTimestepsToFuture
+from datetime import timedelta
+from ocf_datapipes.batch import MergeNumpyModalities
+from ocf_datapipes.utils.consts import BatchKey
 
-
-def test_add_sun_position_hrvsatellite(sat_hrv_dp):
-    pass
+def test_add_sun_position_all(sat_hrv_np_dp, gsp_np_dp, passiv_np_dp):
+    combined_dp = MergeNumpyModalities([sat_hrv_np_dp, gsp_np_dp, passiv_np_dp])
+    combined_dp = AlignGSPto5Min(combined_dp, batch_key_for_5_min_datetimes=BatchKey.hrvsatellite_time_utc)
+    combined_dp = EncodeSpaceTime(combined_dp)
+    combined_dp = SaveT0Time(combined_dp)
+    combined_dp = AddSunPosition(combined_dp, modality_name="hrvsatellite")
+    combined_dp = AddSunPosition(combined_dp, modality_name="gsp")
+    combined_dp = AddSunPosition(combined_dp, modality_name="gsp_5_min")
+    combined_dp = AddSunPosition(combined_dp, modality_name="pv")
+    # TODO Add NWP target time here
+    # TODO Need a locationPicker here to bring down to a few specific locations, GSP has too many locations for this
+    data = next(iter(combined_dp))
 
 
 def test_add_sun_position_satellite(sat_dp):
