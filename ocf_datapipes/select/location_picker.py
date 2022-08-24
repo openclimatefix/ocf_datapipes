3fd12f10b9aef5c345af9840b6458c5f3aecfc1b
Jacob Bieker <jacob@bieker.tech>
2022-08-24 09:43:33 +0100

    Use const for Location, as suggested

--- ocf_datapipes/select/location_picker.py
+++ ocf_datapipes/select/location_picker.py
@@ -1,6 +1,7 @@
 import numpy as np
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
+from ocf_datapipes.utils.consts import Location
 
 
 @functional_datapipe("location_picker")
@@ -10,19 +11,19 @@ class LocationPickerIterDataPipe(IterDataPipe):
         self.source_dp = source_dp
         self.return_all_locations = return_all_locations
 
-    def __iter__(self):
+    def __iter__(self) -> Location:
         for xr_dataset in self.source_dp:
             if self.return_all_locations:
                 # Iterate through all locations in dataset
                 for location_idx in range(len(xr_dataset["x_osgb"])):
-                    location = (
-                        xr_dataset["x_osgb"][location_idx],
-                        xr_dataset["y_osgb"][location_idx],
+                    location = Location(
+                        x=xr_dataset["x_osgb"][location_idx],
+                        y=xr_dataset["y_osgb"][location_idx],
                     )
                     yield location
             else:
                 # Assumes all datasets have osgb coordinates for selecting locations
                 # Pick 1 random location from the input dataset
                 location_idx = np.random.randint(0, len(xr_dataset["x_osgb"]))
-                location = (xr_dataset["x_osgb"][location_idx], xr_dataset["y_osgb"][location_idx])
+                location = Location(x=xr_dataset["x_osgb"][location_idx], y=xr_dataset["y_osgb"][location_idx])
                 yield location
