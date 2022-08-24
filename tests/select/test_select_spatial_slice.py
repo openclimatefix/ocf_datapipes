aa98e7ac4b5c2b2c909a74615d20057fd3decdc7
Jacob Bieker <jacob@bieker.tech>
2022-08-24 11:42:54 +0100

    Update test

--- tests/select/test_select_spatial_slice.py
+++ tests/select/test_select_spatial_slice.py
@@ -4,7 +4,6 @@ def test_select_spatial_slice_meters_passiv(passiv_dp):
     loc_dp = LocationPicker(passiv_dp)
     passiv_dp = SelectSpatialSliceMeters(passiv_dp, location_datapipe=loc_dp, roi_width_meters=96_000, roi_height_meters=96_000)
     data = next(iter(passiv_dp))
-    print(data)
     assert data is not None
 
 def test_select_spatial_slice_pixels_hrv(sat_hrv_dp, passiv_dp):
@@ -14,4 +13,6 @@ def test_select_spatial_slice_pixels_hrv(sat_hrv_dp, passiv_dp):
                                           y_dim_name="y_geostationary",
                                           x_dim_name="x_geostationary")
     data = next(iter(sat_hrv_dp))
-    print(data)
+    assert len(data.x_geostationary) == 256
+    assert len(data.y_geostationary) == 128
+
