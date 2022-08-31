de25c8338bafd3413b306aeae03125195060e2df
Jacob Bieker <jacob@bieker.tech>
2022-08-31 15:52:32 +0100

    Make simpler

--- ocf_datapipes/experimental/ensure_n_nwp_variables.py
+++ ocf_datapipes/experimental/ensure_n_nwp_variables.py
@@ -13,8 +13,6 @@ class EnsureNNWPVariables(IterDataPipe):
     def __iter__(self):
         for np_batch in self.source_datapipe:
             num_tiles = int(np.ceil(self.num_variables / np_batch[BatchKey.nwp_channel_names]))
-            channel_names = np.tile(np_batch[BatchKey.nwp_channel_names], num_tiles)[:self.num_variables]
-            data = np.tile(np_batch[BatchKey.nwp], (1,1,num_tiles,1,1))[:,:,:self.num_variables,:,:]
-            np_batch[BatchKey.nwp] = data
-            np_batch[BatchKey.nwp_channel_names] = channel_names
+            np_batch[BatchKey.nwp_channel_names] = np.tile(np_batch[BatchKey.nwp_channel_names], num_tiles)[:self.num_variables]
+            np_batch[BatchKey.nwp] = np.tile(np_batch[BatchKey.nwp], (1,1,num_tiles,1,1))[:,:,:self.num_variables,:,:]
             yield np_batch
