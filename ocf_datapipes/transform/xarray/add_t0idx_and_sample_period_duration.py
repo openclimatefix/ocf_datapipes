7d36094df4f15ba694299e4c291f54ed44ad64c5
Jacob Bieker <jacob@bieker.tech>
2022-09-01 10:01:53 +0100

    Update PVRollingPowerWindow and docstrings

--- ocf_datapipes/transform/xarray/add_t0idx_and_sample_period_duration.py
+++ ocf_datapipes/transform/xarray/add_t0idx_and_sample_period_duration.py
@@ -3,17 +3,27 @@ from typing import Union
 import xarray as xr
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
-
+from datetime import timedelta
 
 @functional_datapipe("add_t0_idx_and_sample_period_duration")
 class AddT0IdxAndSamplePeriodDurationIterDataPipe(IterDataPipe):
-    def __init__(self, source_datapipe: IterDataPipe, sample_period_duration, history_duration):
+    """Add t0_idx and sample_period_duration attributes to datasets for downstream tasks"""
+    def __init__(self, source_datapipe: IterDataPipe, sample_period_duration: timedelta, history_duration: timedelta):
+        """
+        Adds two attributes, t0_idx, and sample_period_duration for downstream datapipes to use
+
+        Args:
+            source_datapipe: Datapipe emitting a Xarray DataSet or DataArray
+            sample_period_duration: Time between samples
+            history_duration: Amount of history in each example
+        """
         self.source_datapipe = source_datapipe
         self.sample_period_duration = sample_period_duration
         self.history_duration = history_duration
         self.t0_idx = int(self.history_duration / self.sample_period_duration)
 
     def __iter__(self) -> Union[xr.DataArray, xr.Dataset]:
+        """Adds the two attributes to the xarray objects and returns them"""
         for xr_data in self.source_datapipe:
             xr_data.attrs["t0_idx"] = self.t0_idx
             xr_data.attrs["sample_period_duration"] = self.sample_period_duration
--- tests/transform/xarray/test_pv_power_rolling_window.py
+++ tests/transform/xarray/test_pv_power_rolling_window.py
@@ -9,6 +9,7 @@ def test_pv_power_rolling_window_passiv(passiv_dp):
         history_duration=timedelta(minutes=60),
         sample_period_duration=timedelta(minutes=5),
     )
+    data_before = next(iter(passiv_dp))
     passiv_dp = PVPowerRollingWindow(passiv_dp, expect_dataset=False)
     data = next(iter(passiv_dp))
-    assert data is not None
+    assert len(data.time_utc) == len(data_before.time_utc)
