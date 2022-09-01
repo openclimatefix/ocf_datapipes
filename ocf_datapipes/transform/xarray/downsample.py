dcbe63c3fd4deb08d35b1047540b60d7e9cb50a4
Jacob Bieker <jacob@bieker.tech>
2022-09-01 09:43:51 +0100

    Update Downsample to handle other dim names

--- ocf_datapipes/transform/xarray/downsample.py
+++ ocf_datapipes/transform/xarray/downsample.py
@@ -4,16 +4,29 @@ from torchdata.datapipes.iter import IterDataPipe
 
 @functional_datapipe("downsample")
 class DownsampleIterDataPipe(IterDataPipe):
-    def __init__(self, source_dp: IterDataPipe, y_coarsen: int, x_coarsen):
-        self.source_dp = source_dp
+    """Downsample Xarray dataset with coarsen"""
+    def __init__(self, source_datapipe: IterDataPipe, y_coarsen: int, x_coarsen: int, x_dim_name: str = "x_osgb", y_dim_name: str = "y_osgb"):
+        """
+        Downsample xarray dataset/dataarrays with coarsen
+
+        Args:
+            source_datapipe: Datapipe emitting Xarray dataset
+            y_coarsen: Coarsen value in the y direction
+            x_coarsen: Coarsen value in the x direction
+            x_dim_name: X dimension name
+            y_dim_name: Y dimension name
+        """
+        self.source_datapipe = source_datapipe
         self.y_coarsen = y_coarsen
         self.x_coarsen = x_coarsen
+        self.x_dim_name = x_dim_name
+        self.y_dim_name = y_dim_name
 
     def __iter__(self):
-        # TODO Change to lat/lon when doing that
-        for xr_data in self.source_dp:
-            yield xr_data.coarsen(
-                y_osgb=self.y_coarsen,
-                x_osgb=self.x_coarsen,
+        """Coarsen the data on the specified dimensions"""
+        for xr_data in self.source_datapipe:
+            yield xr_data.coarsen({
+                self.y_dim_name: self.y_coarsen,
+                self.x_dim_name: self.x_coarsen,},
                 boundary="trim",
             ).mean()
