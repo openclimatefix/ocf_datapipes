ca22a6830325295b4272d14c3d41e2815b176f53
Jacob Bieker <jacob@bieker.tech>
2022-09-01 10:37:30 +0100

    Add tests for NaNs

--- tests/validation/test_check_for_nans.py
+++ tests/validation/test_check_for_nans.py
@@ -1,5 +1,13 @@
 from ocf_datapipes.validation import CheckNaNs
+import pytest
+import numpy as np
 
+def test_check_nans_fail_dataarray(topo_dp):
+    topo_dp = CheckNaNs(topo_dp)
+    with pytest.raises(Exception):
+        next(iter(topo_dp))
 
-def test_check_nans():
-    pass
+def test_check_nans_fill(topo_dp):
+    topo_dp = CheckNaNs(topo_dp, fill_nans=True)
+    data = next(iter(topo_dp))
+    assert np.all(data != np.nan)
