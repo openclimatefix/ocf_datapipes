246a4cd785adb6a7876015cc2508a5424f05b45a
BumpVersion Action <bumpversion@github-actions>
2022-09-30 08:27:19 +0000

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="ocf_datapipes",
-    version="0.3.0",
+    version="0.3.1",
     license="MIT",
     description="Pytorch Datapipes built primarily for use in Open Climate Fix's nowcasting/forecasting work",
     author="Jacob Bieker, Jack Kelly, Peter Dudfield",
