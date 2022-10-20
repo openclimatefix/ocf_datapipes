08de9487c1576196263b39a335eec0d6196c1513
peterdudfield <peter.dudfield@hotmail.com>
2022-10-20 12:24:53 +0100

    add option to return all datetimes

--- ocf_datapipes/select/select_t0_time.py
+++ ocf_datapipes/select/select_t0_time.py
@@ -1,5 +1,6 @@
 """Select the t0 time for training"""
 import logging
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -13,26 +14,43 @@ logger = logging.getLogger(__name__)
 class SelectT0TimeIterDataPipe(IterDataPipe):
     """Select the random t0 time for the training data"""
 
-    def __init__(self, source_datapipe: IterDataPipe, dim_name: str = "time_utc"):
+    def __init__(
+        self,
+        source_datapipe: IterDataPipe,
+        dim_name: str = "time_utc",
+        return_all_times: Optional[bool] = False,
+    ):
         """
         Select a random t0 time for training
 
         Args:
             source_datapipe: Datapipe emitting Xarray objects
             dim_name: The time dimension name to use
+            return_all_times: option to
         """
         self.source_datapipe = source_datapipe
         self.dim_name = dim_name
+        self.return_all_times = return_all_times
+
+        if self.return_all_times:
+            logger.debug('Will be returning all t0 times')
 
     def __iter__(self) -> pd.Timestamp:
         """Get the latest timestamp and return it"""
         for xr_data in self.source_datapipe:
 
-            logger.debug(f"Selecting t0 from {len(xr_data[self.dim_name])} datetimes")
+            if self.return_all_times:
 
-            if len(xr_data[self.dim_name].values) == 0:
-                assert Exception("There are no values to get t0 from")
-            t0 = np.random.choice(xr_data[self.dim_name].values)
-            logger.debug(f"t0 will be {t0}")
+                for t0 in xr_data[self.dim_name].values:
+                    yield t0
 
-            yield t0
+            else:
+
+                logger.debug(f"Selecting t0 from {len(xr_data[self.dim_name])} datetimes")
+
+                if len(xr_data[self.dim_name].values) == 0:
+                    assert Exception("There are no values to get t0 from")
+                t0 = np.random.choice(xr_data[self.dim_name].values)
+                logger.debug(f"t0 will be {t0}")
+
+                yield t0
