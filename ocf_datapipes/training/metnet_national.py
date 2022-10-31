276f367742377eccad7e5cbcb4b9d198e9479b55
Jacob Bieker <jacob@bieker.tech>
2022-10-31 14:15:43 +0000

    Minor updates

--- ocf_datapipes/training/metnet_national.py
+++ ocf_datapipes/training/metnet_national.py
@@ -136,7 +136,7 @@ def metnet_national_datapipe(
         )
 
         nwp_time_periods_datapipe = nwp_time_periods_datapipe.get_contiguous_time_periods(
-            sample_period_duration=timedelta(minutes=60),
+            sample_period_duration=timedelta(hours=1),
             history_duration=timedelta(minutes=configuration.input_data.nwp.history_minutes),
             forecast_duration=timedelta(minutes=configuration.input_data.nwp.forecast_minutes),
             time_dim="init_time_utc",
