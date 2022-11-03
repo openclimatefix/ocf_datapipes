4b30e535cf8f6f9d0dd51a3f1ffdc1745557df57
Jacob Bieker <jacob@bieker.tech>
2022-11-03 11:45:12 +0000

    Change test

--- tests/training/test_metnet_national.py
+++ tests/training/test_metnet_national.py
@@ -1,14 +1,15 @@
 import os
 
+import torch
+
 import ocf_datapipes
 from ocf_datapipes.training.metnet_national import metnet_national_datapipe
-from ocf_datapipes.utils.consts import BatchKey
 
 
 def test_metnet_datapipe():
     filename = os.path.join(os.path.dirname(ocf_datapipes.__file__), "../tests/config/test.yaml")
-    gsp_datapipe = metnet_national_datapipe(filename)
+    gsp_datapipe = metnet_national_datapipe(filename, use_nwp=False)
 
     batch = next(iter(gsp_datapipe))
-    print(batch[0].shape)
-    print(batch[1].shape)
+    assert all(torch.isfinite(batch[0]))
+    assert all(torch.isfinite(batch[1]))
