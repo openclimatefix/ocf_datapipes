bc43db5facd6e55b50ac2aa81e3d91a1eb74ace3
peterdudfield <peter.dudfield@hotmail.com>
2022-11-22 13:29:17 +0000

    isort

--- ocf_datapipes/training/gsp_pv_satellite_nwp.py
+++ ocf_datapipes/training/gsp_pv_satellite_nwp.py
@@ -10,7 +10,7 @@ from torchdata.datapipes.iter import IterDataPipe
 import ocf_datapipes  # noqa
 from ocf_datapipes.batch import MergeNumpyModalities
 from ocf_datapipes.config.model import Configuration
-from ocf_datapipes.load import OpenConfiguration, OpenNWP, OpenPVFromNetCDF, OpenGSP, OpenSatellite
+from ocf_datapipes.load import OpenConfiguration, OpenGSP, OpenNWP, OpenPVFromNetCDF, OpenSatellite
 from ocf_datapipes.utils.consts import NWP_MEAN, NWP_STD
 
 logger = logging.getLogger(__name__)
