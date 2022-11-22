f19eb93d8474f548ec8dcf549217e5e3c6e6d908
peterdudfield <peter.dudfield@hotmail.com>
2022-11-22 18:08:34 +0000

    add fake datapipeline

--- tests/batch/fake/test_make_fake_batch.py
+++ tests/batch/fake/test_make_fake_batch.py
@@ -1,5 +1,7 @@
-from ocf_datapipes.batch.fake.fake_batch import make_fake_batch
+from ocf_datapipes.batch.fake.fake_batch import make_fake_batch, fake_data_pipeline
 from ocf_datapipes.config.model import Configuration
+
+from torch.utils.data import DataLoader
 
 
 def test_make_fake_batch():
@@ -48,3 +50,16 @@ def test_make_fake_batch_only_nwp_gsp():
     configuration.input_data.gsp = input_data.gsp
 
     _ = make_fake_batch(configuration=configuration)
+
+
+def test_fake_data_pipeline():
+
+    configuration = Configuration()
+    input_data = configuration.input_data.set_all_to_defaults()
+    configuration.input_data.nwp = input_data.nwp
+    configuration.input_data.gsp = input_data.gsp
+
+    data_pipeline = fake_data_pipeline(configuration=configuration)
+    train_dataloader = DataLoader(dataset=data_pipeline, batch_size=None)
+    train_dataloader_iter = iter(train_dataloader)
+    batch = next(train_dataloader_iter)
