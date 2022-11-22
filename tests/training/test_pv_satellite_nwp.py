a6eb4e2f3a09d2487511a1976d525f71e622a076
peterdudfield <peter.dudfield@hotmail.com>
2022-11-22 14:13:16 +0000

    add sbri pipeline

---
+++ tests/training/test_pv_satellite_nwp.py
@@ -0,0 +1,13 @@
+from ocf_datapipes.training.pv_satellite_nwp import pv_nwp_satellite_data_pipeline
+from ocf_datapipes.utils.consts import BatchKey
+
+
+def test_gsp_pv_nwp_satellite_datapipe(configuration_filename):
+
+    pv_datapipe = pv_nwp_satellite_data_pipeline(configuration_filename)
+
+    batch = next(iter(pv_datapipe))
+
+    # 4 in past, now, and 2 in the future
+    print(batch)
+    assert len(batch[BatchKey.pv_time_utc][0]) == 37
