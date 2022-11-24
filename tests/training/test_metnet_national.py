439f6ee6a60e6b04c595f6d8305ea9d5597802cf
Jacob Bieker <jacob@bieker.tech>
2022-11-24 17:52:47 +0000

    Fix test

--- tests/training/test_metnet_national.py
+++ tests/training/test_metnet_national.py
@@ -8,7 +8,7 @@ from ocf_datapipes.training.metnet_national import metnet_national_datapipe
 
 def test_metnet_datapipe():
     filename = os.path.join(os.path.dirname(ocf_datapipes.__file__), "../tests/config/test.yaml")
-    gsp_datapipe = metnet_national_datapipe(filename)
+    gsp_datapipe = metnet_national_datapipe(filename, max_num_pv_systems=10)
 
     batch = next(iter(gsp_datapipe))
     assert np.isfinite(batch[0]).all()
