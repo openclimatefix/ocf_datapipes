d93819bf34e6fb2fc7ecea9b5f2fce8d53c5e6e8
vrym2 <vrym2@student.le.ac.uk>
2022-12-05 21:17:08 +0000

    drop_over_night_tidying

--- ocf_datapipes/select/drop_overnight_pv.py
+++ ocf_datapipes/select/drop_overnight_pv.py
@@ -35,12 +35,15 @@ December	07:57 am	03:53 pm	7:56 h
 
 
 @functional_datapipe("select_pv_systems_with_night_output")
-class PVSystemsIDsWithNightOutputIterDataPipe(IterDataPipe):
+class XarrayDatasetAfterNighttimeVarsDroppedIterDataPipe(IterDataPipe):
     """
     Select systems IDs with pv output at the night time
     """
 
-    def __init__(self, source_datapipe: IterDataPipe, meta_datapipe: IterDataPipe):
+    def __init__(
+        self,
+        source_datapipe: IterDataPipe,
+        meta_datapipe: IterDataPipe):
         """
         This function provides all the system ids which are giving pv output at the night time
 
@@ -48,43 +51,53 @@ class PVSystemsIDsWithNightOutputIterDataPipe(IterDataPipe):
             source_datapipe: A datapipe that emmits Xarray Dataset of the pv.netcdf file
         """
         self.source_datapipe = source_datapipe
-        self.meta_datapipe = meta_datapipe
-
-    def __iter__(self) -> pd.DataFrame():
-        """Function provides ssid's with night time pv output"""
-        # Classifying the night time
+    
+    def __iter__(
+        self)-> xr.Dataset():
+        """Function provides ssid's with night time pv output
+        """
+        #Classifying the night time
         for xr_dataset in self.source_datapipe:
-            ssid = random.choice(list(xr_dataset))
-            df = xr_dataset[ssid].to_dataframe()
-            df["year"] = df.index.year.astype(int)
-            df["month"] = df.index.month.astype(int)
-            df["day"] = df.index.day.astype(int)
-            df["time"] = df.index.time.astype(datetime)
-            df["hr"] = df.index.strftime("%H").astype(int)
-            df["status"] = df[["month", "hr"]].apply(
-                lambda x: "night"  # condtion based on the above data from the comments
-                if (x.month == 1 and (x.hr > 17 and x.hr < 7))
-                else "day"
-                if (x.month == 2) and (x.hr > 18 and x.hr < 7)
-                else "day"
-                if (x.month == 3) and (x.hr > 19 and x.hr < 6)
-                else "day"
-                if (x.month == 4) and (x.hr > 20 and x.hr < 6)
-                else "day"
-                if (x.month == 5) and (x.hr > 21 and x.hr < 5)
-                else "day"
-                if (x.month == 6) and (x.hr > 22 and x.hr < 4)
-                else "day"
-                if (x.month == 7) and (x.hr > 22 and x.hr < 5)
-                else "day"
-                if (x.month == 8) and (x.hr > 21 and x.hr < 5)
-                else "day"
-                if (x.month == 9) and (x.hr > 20 and x.hr < 6)
-                else "day"
-                if (x.month == 10) and (x.hr > 21 and x.hr < 7)
-                else "day"
-                if (x.month == 11) and (x.hr > 17 and x.hr < 7)
-                else "day"
-                if (x.month == 12) and (x.hr > 17 and x.hr < 7)
-                else "day"
-            )
+            dates_list = xr_dataset.coords['datetime'].values
+
+            logger.warning("This dropping of the nighttime pv is only applicable to the UK PV datasets")
+            def day_status(
+                date_time_of_day: datetime
+                ):
+                dtime = date_time_of_day
+                date_month = dtime.month.astype(int)
+                date_hr = dtime.strftime("%H").astype(int)
+                status_day = 'night'
+                if (date_month == 1 and (date_hr!>17 and date_hr!<7) ):
+                    status_day = 'day'
+                elif (date_month == 2 and (date_hr!>18 and date_hr!<7)):
+                    status_day = 'day'
+                elif (date_month == 3 and (date_hr!>19 and date_hr!<6)):
+                    status_day = 'day'
+                elif (date_month == 4 and (date_hr!>20 and date_hr!<6)):
+                    status_day = 'day'
+                elif (date_month == 5 and (date_hr!>21 and date_hr!<5)):
+                    status_day = 'day'
+                elif (date_month == 6 and (date_hr!>22 and date_hr!<4)):
+                    status_day = 'day'
+                elif (date_month == 7 and (date_hr!>22 and date_hr!<5)):
+                    status_day = 'day'
+                elif (date_month == 8 and (date_hr!>21 and date_hr!<5)):
+                    status_day = 'day'
+                elif (date_month == 9 and (date_hr!>20 and date_hr!<6)):
+                    status_day = 'day'
+                elif (date_month == 10 and (date_hr!>21 and date_hr!<7)):
+                    status_day = 'day'
+                elif (date_month == 11 and (date_hr!>17 and date_hr!<7)):
+                    status_day = 'day'
+                elif (date_month == 12 and (date_hr!>17 and date_hr!<7)):
+                    status_day = 'day'
+                else:
+                    logger.debug(f"The datetime {dtime} is not an appropriate date")
+                return status_day
+            
+            status = [day_status(i) for i in dates_list]
+            assert len(dates_list) == len(status)
+            xr_dataset = xr_dataset.assign_ccords(daynight_status = ("datetime",status))
+            xr_dataset = xr_dataset.where(xr_dataset.daynight_status == "day")
+            return xr_dataset
