966a55608cc7e14b841a15b00e28b10a8d4b1d70
vrym2 <vrym2@student.le.ac.uk>
2022-12-07 13:59:37 +0000

    testing with simole datapipe successful

--- tests/select/test_drop_over_night_pv.py
+++ tests/select/test_drop_over_night_pv.py
@@ -3,3 +3,5 @@ from ocf_datapipes.select import DropNightPV
 
 def test_drop_night_pv(simple_netcdf_datapipe):
     night_drop_pv = DropNightPV(simple_netcdf_datapipe)
+    data = len(next(iter(night_drop_pv)))
+    assert data is not None
