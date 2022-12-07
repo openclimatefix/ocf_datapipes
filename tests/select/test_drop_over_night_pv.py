99d3ed734166e8544309d388af9b3658985e4169
vrym2 <vrym2@student.le.ac.uk>
2022-12-07 16:28:02 +0000

    tidying the code and testing

--- tests/select/test_drop_over_night_pv.py
+++ tests/select/test_drop_over_night_pv.py
@@ -3,5 +3,5 @@ from ocf_datapipes.select import DropNightPV
 
 def test_drop_night_pv(simple_netcdf_datapipe):
     night_drop_pv = DropNightPV(simple_netcdf_datapipe)
-    data = len(next(iter(night_drop_pv)))
-    assert data is not None
+    data = next(iter(night_drop_pv))
+    assert 'day' or 'night' in data.coords["daynight_status"].values
