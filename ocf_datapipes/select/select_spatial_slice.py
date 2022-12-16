83c481e4c748da2bc50b85a05ae30e9ef44c6d0d
peterdudfield <peter.dudfield@hotmail.com>
2022-12-16 15:12:12 +0000

    update for select_spatial_slice

--- ocf_datapipes/select/select_spatial_slice.py
+++ ocf_datapipes/select/select_spatial_slice.py
@@ -27,6 +27,7 @@ class SelectSpatialSlicePixelsIterDataPipe(IterDataPipe):
         roi_width_pixels: int,
         y_dim_name: str = "y",
         x_dim_name: str = "x",
+        datapipe_name: str = 'not-set'
     ):
         """
         Select spatial slice based off pixels from point of interest
@@ -38,6 +39,7 @@ class SelectSpatialSlicePixelsIterDataPipe(IterDataPipe):
             roi_width_pixels: ROI width in pixels
             y_dim_name: Dimension name for Y
             x_dim_name: Dimension name for X
+            datapipe_name: data pipe name, use for profiling
         """
         self.source_datapipe = source_datapipe
         self.location_datapipe = location_datapipe
@@ -45,11 +47,12 @@ class SelectSpatialSlicePixelsIterDataPipe(IterDataPipe):
         self.roi_width_pixels = roi_width_pixels
         self.y_dim_name = y_dim_name
         self.x_dim_name = x_dim_name
+        self.datapipe_name = datapipe_name
 
     def __iter__(self) -> Union[xr.DataArray, xr.Dataset]:
         for xr_data, location in self.source_datapipe.zip_ocf(self.location_datapipe):
 
-            with profile('select_spatial_slice_pixels'):
+            with profile(f'select_spatial_slice_pixels {self.datapipe_name}'):
 
                 if "geostationary" in self.x_dim_name:
                     center_idx: Location = _get_idx_of_pixel_closest_to_poi_geostationary(
