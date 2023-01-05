c9173e91adbdde85ea91981a0c377883999ac6ee
Jacob Bieker <jacob@bieker.tech>
2023-01-05 09:39:36 +0000

    Fix #133

--- ocf_datapipes/load/satellite.py
+++ ocf_datapipes/load/satellite.py
@@ -8,6 +8,7 @@ import pandas as pd
 import xarray as xr
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
+from jpeg_xl_float_with_nans import jpeg_xl_float_with_nans
 
 _log = logging.getLogger(__name__)
 
