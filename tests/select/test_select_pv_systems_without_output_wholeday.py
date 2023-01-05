5b981b9ed9b1436a7416e28248d46bcc96140d80
vrym2 <vrym2@student.le.ac.uk>
2023-01-05 16:43:51 +0000

    added a test to check the execution time

--- tests/select/test_select_pv_systems_without_output_wholeday.py
+++ tests/select/test_select_pv_systems_without_output_wholeday.py
@@ -1,8 +1,35 @@
+import logging
+from timeit import timeit
+
 import numpy as np
 import pandas as pd
 import xarray as xr
 
+logger = logging.getLogger(__name__)
+
 from ocf_datapipes.select import DropPVSysWithOnlyNanInADay, TrimDatesWithInsufficentData
+
+
+def test_execution_time():
+    time = pd.date_range(start="2022-01-01", end="2023-01-01", freq="5T")
+    pv_system_id = [1, 2, 3]
+    ALL_COORDS = {"time_utc": time, "pv_system_id": pv_system_id}
+
+    data = np.zeros((len(time), len(pv_system_id)))
+    data[:, 2] = np.nan
+
+    data_array = xr.DataArray(
+        data,
+        coords=ALL_COORDS,
+    )
+    drop_sys_with_only_nan = DropPVSysWithOnlyNanInADay(
+        [data_array], minimum_number_data_points=288
+    )
+    data_after_drop = next(iter(drop_sys_with_only_nan))
+    execution_time = timeit(lambda: next(iter((data_after_drop))), number=1)
+    logger.info("Testing the execution time for a year of data")
+    logger.info(f"Execution time to test:{execution_time:.4f} seconds")
+    print(f"Execution time to test for 10k times:{execution_time:.4f} seconds")
 
 
 def test_trim_12th_hour_timestep(passiv_datapipe):
