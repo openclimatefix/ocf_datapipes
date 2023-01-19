c4ca8940099c50c3830e5e6dc6fe42b1fda712b7
peterdudfield <peter.dudfield@hotmail.com>
2023-01-19 14:10:20 +0000

    add xgboost production and test pipeline

---
+++ tests/production/test_xgnational.py
@@ -0,0 +1,22 @@
+import os
+
+import xarray as xr
+from freezegun import freeze_time
+
+import ocf_datapipes
+from ocf_datapipes.production.xgnational import xgnational_production_datapipe
+
+
+@freeze_time("2022-01-01 08:00")
+def test_xgnational_production_datapipe(gsp_yields):
+
+    filename = os.path.join(os.path.dirname(ocf_datapipes.__file__), "../tests/config/test.yaml")
+
+    data = xgnational_production_datapipe(filename)
+
+    gsp_xr = data["gsp"]
+    nwp_xr = data["nwp"]
+
+    assert gsp_xr.gsp_id.values == [0]
+    assert len(gsp_xr.time_utc) == 5
+    assert isinstance(nwp_xr, xr.DataArray)
