7ff1c51d4b3537228d5df44f0ad38dfb55476b13
Jacob Bieker <jacob@bieker.tech>
2023-02-14 09:44:45 +0000

    Add missing key to test

--- tests/training/test_common.py
+++ tests/training/test_common.py
@@ -59,11 +59,11 @@ def test_add_selected_time_slices_from_datapipes():
     used_datapipes = open_and_return_datapipes("tests/config/test.yaml")
     used_datapipes = get_and_return_overlapping_time_periods_and_t0(used_datapipes)
     used_datapipes = add_selected_time_slices_from_datapipes(used_datapipes)
-    for key in ["nwp", "topo", "gsp", "gsp_future", "pv", "sat", "hrv"]:
+    for key in ["nwp", "topo", "gsp", "gsp_future", "pv", "pv_future", "sat", "hrv"]:
         assert key in used_datapipes.keys()
         assert isinstance(used_datapipes[key], IterDataPipe)
     assert sorted(list(used_datapipes.keys())) == sorted(
-        ["nwp", "config", "topo", "gsp", "gsp_future", "pv", "sat", "hrv"]
+        ["nwp", "config", "topo", "gsp", "gsp_future", "pv", "pv_future", "sat", "hrv"]
     )
 
 
@@ -72,15 +72,15 @@ def test_add_selected_time_slices_from_datapipes_fork_iterations():
     used_datapipes = open_and_return_datapipes("tests/config/test.yaml")
     used_datapipes = get_and_return_overlapping_time_periods_and_t0(used_datapipes)
     used_datapipes = add_selected_time_slices_from_datapipes(used_datapipes)
-    for key in ["nwp", "topo", "gsp", "gsp_future", "pv", "sat", "hrv"]:
+    for key in ["nwp", "topo", "gsp", "gsp_future", "pv", "pv_future", "sat", "hrv"]:
         assert key in used_datapipes.keys()
         assert isinstance(used_datapipes[key], IterDataPipe)
     assert sorted(list(used_datapipes.keys())) == sorted(
-        ["nwp", "config", "topo", "gsp", "gsp_future", "pv", "sat", "hrv"]
+        ["nwp", "config", "topo", "gsp", "gsp_future", "pv", "pv_future", "sat", "hrv"]
     )
     # Zip together to see if any are missing
     zipped = Zipper(
-        *[used_datapipes[k] for k in ["nwp", "topo", "gsp", "gsp_future", "pv", "sat", "hrv"]]
+        *[used_datapipes[k] for k in ["nwp", "topo", "gsp", "gsp_future", "pv", "pv_future", "sat", "hrv"]]
     )
     dataloader = DataLoader2(zipped)
     for i, batch in enumerate(dataloader):
