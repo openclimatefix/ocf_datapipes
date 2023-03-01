4bd906655d7207557abf365281e7282077e9de46
James Fulton <djamesfulton@yahoo.co.uk>
2023-03-01 11:24:18 +0000

    remove osgb expectation

--- ocf_datapipes/convert/satellite.py
+++ ocf_datapipes/convert/satellite.py
@@ -22,8 +22,10 @@ class ConvertSatelliteToNumpyBatchIterDataPipe(IterDataPipe):
         self.source_datapipe = source_datapipe
         self.is_hrv = is_hrv
 
+
     def __iter__(self) -> NumpyBatch:
         """Convert each example to a NumpyBatch object"""
+                
         for xr_data in self.source_datapipe:
 
             with profile("convert_satellite_to_numpy_batch"):
@@ -37,8 +39,6 @@ class ConvertSatelliteToNumpyBatchIterDataPipe(IterDataPipe):
                     }
 
                     for batch_key, dataset_key in (
-                        (BatchKey.hrvsatellite_y_osgb, "y_osgb"),
-                        (BatchKey.hrvsatellite_x_osgb, "x_osgb"),
                         (BatchKey.hrvsatellite_y_geostationary, "y_geostationary"),
                         (BatchKey.hrvsatellite_x_geostationary, "x_geostationary"),
                     ):
@@ -54,12 +54,11 @@ class ConvertSatelliteToNumpyBatchIterDataPipe(IterDataPipe):
                     }
 
                     for batch_key, dataset_key in (
-                        (BatchKey.satellite_y_osgb, "y_osgb"),
-                        (BatchKey.satellite_x_osgb, "x_osgb"),
                         (BatchKey.satellite_y_geostationary, "y_geostationary"),
                         (BatchKey.satellite_x_geostationary, "x_geostationary"),
                     ):
                         # HRVSatellite coords are already float32.
                         example[batch_key] = xr_data[dataset_key].values
+
 
                 yield example
