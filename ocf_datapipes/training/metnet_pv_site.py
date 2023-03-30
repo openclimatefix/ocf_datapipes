1a391bca5784f7412cad52732f9ad63e4f694f47
Jacob Bieker <jacob@bieker.tech>
2023-03-30 08:03:06 +0100

    Fix typo

--- ocf_datapipes/training/metnet_pv_site.py
+++ ocf_datapipes/training/metnet_pv_site.py
@@ -103,7 +103,7 @@ def metnet_site_datapipe(
     pv_datapipe = used_datapipes["pv_future"].normalize(normalize_fn=normalize_pv)
     # Split into GSP for target, only national, and one for history
     pv_datapipe, pv_loc_datapipe = pv_datapipe.fork(2)
-    pv_loc_datapipe, pv_id_datapipe = LocationPicker(pv_loc_datapipe).loc_datapipe.fork(2)
+    pv_loc_datapipe, pv_id_datapipe = LocationPicker(pv_loc_datapipe).fork(2)
     pv_history = pv_history.select_id(pv_id_datapipe, data_source_name="pv")
 
     if "nwp" in used_datapipes.keys():
