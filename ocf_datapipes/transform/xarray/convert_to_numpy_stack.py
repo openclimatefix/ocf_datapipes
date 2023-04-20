836f8b03ded6faf608d2c713b2c193f2895e9b5f
Jacob Bieker <jacob@bieker.tech>
2023-04-20 21:39:55 +0100

    Ensure channel is first

--- ocf_datapipes/transform/xarray/convert_to_numpy_stack.py
+++ ocf_datapipes/transform/xarray/convert_to_numpy_stack.py
@@ -30,12 +30,24 @@ class StackXarrayIterDataPipe(IterDataPipe):
         for xr_datas in Zipper(*self.source_datapipes):
             stack = []
             for xr_index, xr_data in enumerate(xr_datas):
+                #print(xr_data.dims)
+                #print(xr_data.shape)
+                if "channel" in xr_data.dims:
+                    channel_idx = xr_data.dims.index("channel")
+                else:
+                    channel_idx = -1
+                if channel_idx != -1 and channel_idx > 0:
+                    xr_data = xr_data.transpose("channel", *xr_data.dims[:channel_idx], *xr_data.dims[channel_idx+1:])
+                    channel_idx = 0
+                #print(xr_data.dims)
                 # Resamples to the same number of pixels for both center and contexts
                 xr_data = xr_data.to_numpy()
+                #print(xr_data.shape)
                 if len(xr_data.shape) == 2:  # Need to add channel dimension
                     xr_data = np.expand_dims(xr_data, axis=0)
                 if len(xr_data.shape) == 3:  # Need to add channel dimension
                     xr_data = np.expand_dims(xr_data, axis=0)
+                #print(xr_data.shape)
                 stack.append(xr_data)
             # Pad out time dimension to be the same, using the largest one
             # All should have 4 dimensions at this point
