d42c14b9b643b53a1b047f090ff3be76449a2ba8
James Fulton <djamesfulton@yahoo.co.uk>
2023-05-16 12:23:16 +0000

    add tests

--- tests/select/test_select_time_slice.py
+++ tests/select/test_select_time_slice.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 import pandas as pd
+import numpy as np
 from torchdata.datapipes.iter import IterableWrapper
 from ocf_datapipes.select import SelectTimeSlice
 
@@ -39,3 +40,31 @@ def test_select_time_slice_sat(sat_datapipe):
     for sat_sample, t0 in zip(sat_samples, t0_values):
         assert len(sat_sample.time_utc) == 3
         assert sat_sample.time_utc[1] == t0
+
+    # Check with out of bounds selection
+    t_last = pd.to_datetime(data.time_utc.values[-1])
+    t0_values = [
+        t_last - timedelta(minutes=5),
+        t_last,
+        t_last + timedelta(minutes=5),
+        t_last + timedelta(minutes=10),
+    ]
+    t0_datapipe = IterableWrapper(t0_values)
+    
+    dp = SelectTimeSlice(
+        sat_datapipe,
+        t0_datapipe,
+        sample_period_duration=timedelta(minutes=5),
+        interval_start=timedelta(minutes=-5),
+        interval_end=timedelta(minutes=5),
+        fill_selection=True,
+    )
+
+    sat_samples = list(dp)
+
+    for i, (sat_sample, t0) in enumerate(zip(sat_samples, t0_values)):
+        assert len(sat_sample.time_utc) == 3
+        assert sat_sample.time_utc[1] == t0
+        # Correct number of time steps are all NaN
+        sat_sel = sat_sample.isel(x_geostationary=0, y_geostationary=0, channel=0)
+        assert np.isnan(sat_sel.values).sum()==i
--- tests/training/test_pvnet.py
+++ tests/training/test_pvnet.py
@@ -36,6 +36,19 @@ def test_construct_sliced_data_pipeline(configuration_filename):
     )
 
     batch = next(iter(dp))
+    
+    
+    # Chosen to lie beyond end of test data
+    t0_pipe = IterableWrapper([datetime(2020, 4, 2, 0, 30)])
+    
+    dp = construct_sliced_data_pipeline(
+        configuration_filename,
+        location_pipe=loc_pipe,
+        t0_datapipe=t0_pipe,
+        production=True,
+    )
+
+    batch = next(iter(dp))
 
 
 def test_pvnet_datapipe(configuration_filename):
