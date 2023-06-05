c3d6070c0e1d14ab2f313295a86ec648de66fd97
peterdudfield <peter.dudfield@hotmail.com>
2023-06-05 12:08:49 +0100

    add pvnet production test back in

--- tests/training/test_pvnet.py
+++ tests/training/test_pvnet.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-import pytest
 from torchdata.datapipes.iter import IterableWrapper
 
 from ocf_datapipes.training.pvnet import (
@@ -36,24 +35,6 @@ def test_construct_sliced_data_pipeline(configuration_filename):
         configuration_filename,
         location_pipe=loc_pipe,
         t0_datapipe=t0_pipe,
-    )
-
-    batch = next(iter(dp))
-
-
-@pytest.mark.skip("Failing at the moment")
-def test_construct_sliced_data_pipeline_outside_test(configuration_filename):
-    # This is randomly chosen, but real, GSP location
-    loc_pipe = IterableWrapper([Location(x=246699.328125, y=849771.9375, id=18)])
-
-    # Chosen to lie beyond end of test data
-    t0_pipe = IterableWrapper([datetime(2020, 4, 2, 0, 30)])
-
-    dp = construct_sliced_data_pipeline(
-        configuration_filename,
-        location_pipe=loc_pipe,
-        t0_datapipe=t0_pipe,
-        production=True,
     )
 
     batch = next(iter(dp))
