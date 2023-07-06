363c73bbdf4673a013644195d9020ff54588a6d6
peterdudfield <peter.dudfield@hotmail.com>
2023-07-06 09:23:02 +0100

    fix test

--- tests/production/test_pvnet_production.py
+++ tests/production/test_pvnet_production.py
@@ -47,12 +47,13 @@ def test_construct_sliced_data_pipeline(configuration_filename, gsp_yields):
     batch = next(iter(dp))
 
 
+@freeze_time("2020-04-01 02:30:00")
 def test_construct_sliced_data_pipeline_satellite_with_zeros(configuration_filename, gsp_yields):
     # This is randomly chosen, but real, GSP location
     loc_pipe = IterableWrapper([Location(x=246699.328125, y=849771.9375, id=18)])
 
     # Data after the test data
-    t0_pipe = IterableWrapper([datetime(2020, 4, 1, 12, 30)])
+    t0_pipe = IterableWrapper([datetime(2020, 4, 1, 16, 30)])
 
     dp = construct_sliced_data_pipeline(
         configuration_filename,
@@ -61,5 +62,5 @@ def test_construct_sliced_data_pipeline_satellite_with_zeros(configuration_filen
         check_satellite_no_zeros=True,
         production=True,
     )
-    with pytest.raises(Exception):
-        batch = next(iter(dp))
+    with pytest.raises(ValueError):
+        _ = next(iter(dp))
