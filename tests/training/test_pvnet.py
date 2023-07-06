589a4881700a106fc8e38d28a16bbd940f6faf33
peterdudfield <peter.dudfield@hotmail.com>
2023-07-06 09:04:33 +0100

    move to test to production

--- tests/training/test_pvnet.py
+++ tests/training/test_pvnet.py
@@ -25,7 +25,6 @@ def test_construct_loctime_pipelines(configuration_filename):
     next(iter(t0_pipe))
 
 
-# TODO currently not stable tests
 def test_construct_sliced_data_pipeline(configuration_filename):
     # This is randomly chosen, but real, GSP location
     loc_pipe = IterableWrapper([Location(x=246699.328125, y=849771.9375, id=18)])
@@ -37,28 +36,9 @@ def test_construct_sliced_data_pipeline(configuration_filename):
         configuration_filename,
         location_pipe=loc_pipe,
         t0_datapipe=t0_pipe,
-        check_satellite_no_zeros=True,
     )
 
-    batch = next(iter(dp))
-
-
-# TODO currently not stable tests
-def test_construct_sliced_data_pipeline_satellite_with_zeros(configuration_filename):
-    # This is randomly chosen, but real, GSP location
-    loc_pipe = IterableWrapper([Location(x=246699.328125, y=849771.9375, id=18)])
-
-    # Data after the test data
-    t0_pipe = IterableWrapper([datetime(2020, 4, 1, 12, 30)])
-
-    dp = construct_sliced_data_pipeline(
-        configuration_filename,
-        location_pipe=loc_pipe,
-        t0_datapipe=t0_pipe,
-        check_satellite_no_zeros=True,
-    )
-    with pytest.raises(Exception):
-        batch = next(iter(dp))
+    _ = next(iter(dp))
 
 
 def test_pvnet_datapipe(configuration_filename):
