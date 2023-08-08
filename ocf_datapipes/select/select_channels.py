aefded6fc1863701f98d66aa6b48a16d15c2b146
Jacob Bieker <jacob@bieker.tech>
2023-08-08 14:02:48 +0100

    Add combining data arrays when selecting channels

--- ocf_datapipes/select/select_channels.py
+++ ocf_datapipes/select/select_channels.py
@@ -18,6 +18,7 @@ class SelectChannelsIterDataPipe(IterDataPipe):
         source_datapipe: IterDataPipe,
         channels: List[str],
         dim_name: str = "channel",
+        combine_data_arrays: bool = False,
     ):
         """
         Select channels
@@ -26,13 +27,24 @@ class SelectChannelsIterDataPipe(IterDataPipe):
             source_datapipe: Datapipe of Xarray objects
             channels: Channel names to keep
             dim_name: Dimension name for time
+            combine_data_arrays: Combine the data arrays into a single DataArray after selecting variables
+                (Useful for NWPs where each variable is its own data variable)
         """
         self.source_datapipe = source_datapipe
         self.channels = channels
         self.dim_name = dim_name
+        self.combine_data_arrays = combine_data_arrays
 
     def __iter__(self) -> Union[xr.DataArray, xr.Dataset]:
         for xr_data in self.source_datapipe:
-            logger.debug(f"Selecting Channels: {self.channels} out of {xr_data['channel'].values}")
+            if self.combine_data_arrays:
+                # Drop data variables whose names are not in channels
+                xr_data = xr_data.drop_vars(
+                    [v for v in xr_data.data_vars if v not in self.channels]
+                )
+                xr_data = xr_data.to_array(dim=self.dim_name)
+            logger.debug(
+                f"Selecting Channels: {self.channels} out of {xr_data['channel'].values}"
+            )
             xr_data = xr_data.sel({self.dim_name: list(self.channels)})
             yield xr_data
