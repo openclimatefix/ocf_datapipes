c7dcc024ee9ee6af3ecd62efcb4752de5fde7f16
James Fulton <djamesfulton@yahoo.co.uk>
2023-08-21 14:54:09 +0000

    consolidate functions

--- ocf_datapipes/transform/xarray/pv/assign_daynight_status.py
+++ ocf_datapipes/transform/xarray/pv/assign_daynight_status.py
@@ -2,14 +2,18 @@
 This is a class function that assigns day night status
 """
 import logging
+from typing import Union
 
 import numpy as np
 import xarray as xr
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
 
+import pvlib
+
 logger = logging.getLogger(__name__)
 
+# {month: [sunrise_hour, sunset_hour]}
 uk_daynight_dict = {
     1: [7, 17],
     2: [7, 18],
@@ -24,49 +28,143 @@ uk_daynight_dict = {
     11: [7, 17],
     12: [7, 17],
 }
+
 logger.info(
     f"The day and night standard hours are set by {'https://www.timeanddate.com/sun/uk/london'}"
 )
+
+
+@np.vectorize
+def month_to_dawn_hour(month):
+    """Function returning the first hour after dawn for the given month.
+    
+    Args:
+        month: Array-like of ints or int month number in range [1, 12]
+        
+    Returns:
+        Array of the first hour of the day
+    
+    """
+    return uk_daynight_dict[month][0]
+    
+
+@np.vectorize
+def month_to_dusk_hour(month):
+    """Function returning the first hour after dusk for the given month.
+    
+    Args:
+        month: Array-like of ints or int month number in range [1, 12]
+        
+    Returns:
+        Array of the first hour of the night
+    
+    """
+    return uk_daynight_dict[month][1]
 
 
 @functional_datapipe("assign_daynight_status")
 class AssignDayNightStatusIterDataPipe(IterDataPipe):
     """Adds a new dimension of day/night status"""
 
-    def __init__(self, source_datapipe: IterDataPipe):
+    def __init__(
+        self, 
+        source_datapipe: IterDataPipe,
+        method: Union["simple", "elevation"] = "simple",
+    ):
         """This method adds extra coordinate of day night status.
 
         Args:
-            source_datapipe: Datapipe emiiting Xarray Dataset
-        Result:
-            <xarray.Dataset>
-            Dimensions:   (datetime: 289)
-            Coordinates:
-            * datetime  (datetime) datetime64[ns]
-            * status_day (datetime)
+            source_datapipe: Datapipe emitting Xarray Dataset with time_utc coordinate
+            method: Method used to assign the day-night status. Either "simple" or "elevation"
+            
+        Notes:
+            If method is 'simple', then the month and hour are used to decide which datetimes
+            correspond to day and night. These are done via a lookup table compiled for UK-London. 
+            These will be inappropriate far from London. 
+            
+            If method is 'elevation', then the sun elevation is calculated for each datetime and PV
+            system using its latitude and longitude. This is computationally expensive.
         """
 
         self.source_datapipe = source_datapipe
+        self.method = method
+        
+        if method=='simple':
+            logger.warning(
+                "Calculating the day-night status using method 'simple' is only appropriate for "
+                "UK PV datasets"
+            )
+            self._status_func = self._status_by_hour
+            
+        elif method=="elevation":
+            logger.warning(
+                "Calculating the day-night status using method 'elevation' can take a long time"
+            )
+            self._status_func = self._status_by_elevation
+        else:
+            raise ValueError(f"Method '{method}' not recognised")
 
+    
+    def _get_status_by_hour(self, ds):
+        # Getting month and hour values from time_utc and stacking them
+        months = ds.time_utc.dt.month.values
+        hours = ds.time_utc.dt.hour.values
+
+        # Getting the status of day/night for each timestamp in the dates
+        is_after_dawn = hours >= month_to_dawn_hour(months)
+        is_before_dusk = hours < month_to_dusk_hour(months)
+
+        status_daynight = np.where(
+            np.logical_and(is_after_dawn, is_before_dusk),
+            "day",
+            "night",
+        )
+
+        # Assigning a new coordinates of 'status_daynight' in the DataArray
+        ds = ds.assign_coords(status_daynight=(("time_utc"), status_daynight))
+        return ds
+    
+    
+    def _status_by_elevation(self, ds):
+        
+        elevation = xr.full_like(ds, fill_value=np.nan).astype(np.float32)
+
+        for system_id in ds.pv_system_id.values[:5]:
+            ds_sel = ds.sel(pv_system_id=system_id)
+            
+            solpos = pvlib.solarposition.get_solarposition(
+                time=ds_sel.time_utc,
+                latitude=ds_sel.latitude.item(),
+                longitude=ds_sel.longitude.item(),
+                #method="nrel_numba",
+                # Which `method` to use?
+                # pyephem seemed to be a good mix between speed and ease but causes
+                # segfaults!
+                # nrel_numba doesn't work when using multiple worker processes.
+                # nrel_c is probably fastest but requires C code to be manually compiled:
+                # https://midcdmz.nrel.gov/spa/
+            )
+            elevation.sel(pv_system_id=system_id).values[:] = solpos["elevation"]
+
+        status_daynight = np.where(
+            elevation>0.,
+            "day",
+            "night",
+        )
+
+        # Assigning a new coordinates of 'status_daynight' in the DataArray
+        ds = ds.assign_coords(status_daynight=(("time_utc"), status_daynight))
+        return ds
+
+    
     def __iter__(self) -> xr.DataArray():
-        """Returns an xarray dataset with extra dimesion"""
+        """Adds extra coordinate of day-night status
+        
+        Returns:
+            Dataset with additional coordinate describing the datetime as either 'day' or 'night'
+        """
 
         # Reading the Xarray dataset
-        for xr_dataset in self.source_datapipe:
-            # Getting Month and Hour values from time_utc and stacking them
-            date_month = xr_dataset.time_utc.dt.month.values
-            date_hr = xr_dataset.time_utc.dt.hour.values
-            month_hr_stack = np.stack((date_month, date_hr))
-
-            # Getting the status of day/night for each timestamp in the dates'
-            day_start = np.asarray([uk_daynight_dict[m][0] for m in month_hr_stack[0]])
-            day_end = np.asarray([uk_daynight_dict[m][1] for m in month_hr_stack[0]])
-            status_daynight = np.where(
-                np.logical_and(month_hr_stack[1] >= day_start, month_hr_stack[1] < day_end),
-                "day",
-                "night",
-            )
-
-            # Assigning a new coordinates of 'status_daynight' in the DataArray
-            xr_dataset = xr_dataset.assign_coords(status_daynight=(("time_utc"), status_daynight))
-            yield xr_dataset
+        for ds in self.source_datapipe:
+            ds = self._method_fn
+            yield ds
