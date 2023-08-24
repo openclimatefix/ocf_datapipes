77a2554af31489375218bf43d0b97d9bea90187c
James Fulton <djamesfulton@yahoo.co.uk>
2023-08-24 12:05:58 +0000

    fix test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -321,8 +321,8 @@ def pv_netcdf_file():
     - ss_id
     - generation_wh
     """
-
-    datetimes = pd.date_range("2022-09-01 00:00", "2022-09-08 00:00")
+    
+    datetimes = pd.date_range("2022-09-01 00:00", "2022-09-08 00:00", freq="5T")
     pv_system_ids = (np.arange(10) + 9905).astype(str)
 
     data = np.full((len(datetimes), len(pv_system_ids)), fill_value=9.1)
@@ -333,9 +333,9 @@ def pv_netcdf_file():
             ("pv_system_id", pv_system_ids),
         )
     )
-    
-    da = da.where(da.datetime.dt.hour > 21, other=0)
-    da = da.where(da.datetime.dt.hour < 3, other=0)
+
+    da = da.where(da.datetime.dt.hour < 21, other=0)
+    da = da.where(da.datetime.dt.hour > 3, other=0)
     
     da.isel(datetime=slice(0,3), pv_system_id=0).values[:] = np.nan
     ds = da.to_dataset(dim="pv_system_id")
