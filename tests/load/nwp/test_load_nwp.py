ffa32f2b7935a459fe1330ee1027dcb6128f713b
Jacob Bieker <jacob@bieker.tech>
2023-10-16 17:02:44 +0100

    Add test ECMWF data and opening tests

--- tests/load/nwp/test_load_nwp.py
+++ tests/load/nwp/test_load_nwp.py
@@ -27,6 +27,13 @@ def test_load_icon_global():
     metadata = next(iter(nwp_datapipe))
     assert metadata is not None
 
+def test_load_ecmwf():
+    nwp_datapipe = OpenNWP(
+        zarr_path="tests/data/ifs.zarr",
+        provider="ecmwf",
+    )
+    metadata = next(iter(nwp_datapipe))
+    assert metadata is not None
 
 def test_load_latest_nwp():
     base_nwp_datapipe = OpenNWP(zarr_path="tests/data/nwp_data/test.zarr")
