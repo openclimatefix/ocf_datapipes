2a19c0edf7b6f64e8c12ddf4f4b83bfbe86be354
Jacob Bieker <jacob@bieker.tech>
2023-10-24 13:14:08 +0100

    Fix test

--- ocf_datapipes/training/example/nwp_pv.py
+++ ocf_datapipes/training/example/nwp_pv.py
@@ -49,7 +49,7 @@ def nwp_pv_datapipe(
         .fork(2, buffer_size=BUFFER_SIZE)
     )
 
-    if configuration.input_data.nwp.nwp_provider == "UKMetOffice":
+    if configuration.input_data.nwp.nwp_provider == "UKMetOffice" or configuration.input_data.nwp.nwp_provider == "ukv":
         nwp_datapipe = OpenNWPID(configuration.input_data.nwp.nwp_zarr_path)
     elif configuration.input_data.nwp.nwp_provider == "GFS":
         nwp_datapipe = OpenGFSForecast(configuration.input_data.nwp.nwp_zarr_path)
