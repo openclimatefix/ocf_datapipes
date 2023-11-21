023cfc9f76c396831a484b20b95570b67d29e9f2
Jacob Bieker <jacob@bieker.tech>
2023-11-21 13:34:01 +0000

    Add unit test

--- tests/validation/test_check_equality.py
+++ tests/validation/test_check_equality.py
@@ -2,8 +2,9 @@ from ocf_datapipes.validation import (
     CheckGreaterThanOrEqualTo,
     CheckLessThanOrEqualTo,
     CheckNotEqualTo,
+    CheckValueEqualToFraction,
 )
-import numpy as np
+import pytest
 
 
 def test_check_not_equal_to(topo_datapipe):
@@ -19,3 +20,14 @@ def test_check_less_than_or_equal_to(topo_datapipe):
 def test_check_greater_than_or_equal_to(topo_datapipe):
     topo_datapipe = CheckGreaterThanOrEqualTo(topo_datapipe, -9999)
     data = next(iter(topo_datapipe))
+
+
+def test_check_value_equal_to_fraction_no_warning(topo_datapipe):
+    topo_datapipe = CheckValueEqualToFraction(topo_datapipe, value=0.0, fraction=0.1)
+    data = next(iter(topo_datapipe))
+
+
+def test_check_value_equal_to_fraction_with_warning(topo_datapipe):
+    topo_datapipe = CheckValueEqualToFraction(topo_datapipe, value=0.0, fraction=0.0)
+    with pytest.raises(Warning):
+        data = next(iter(topo_datapipe))
