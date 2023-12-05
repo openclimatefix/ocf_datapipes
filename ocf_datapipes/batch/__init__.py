36fb6599382d840168259b785be03e418af9354b
James Fulton <djamesfulton@yahoo.co.uk>
2023-12-05 14:59:50 +0000

    remove unused

--- ocf_datapipes/batch/__init__.py
+++ ocf_datapipes/batch/__init__.py
@@ -1,5 +1,4 @@
 """Datapipes for batching together data"""
-from .merge_numpy_examples_to_batch import (
-    MergeNumpyExamplesToBatchIterDataPipe as MergeNumpyExamplesToBatch,
+from .merge_numpy_modalities import (
+    MergeNumpyModalitiesIterDataPipe as MergeNumpyModalities,
 )
-from .merge_numpy_modalities import MergeNumpyModalitiesIterDataPipe as MergeNumpyModalities
--- ocf_datapipes/fake/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Fake data generators for testing"""
