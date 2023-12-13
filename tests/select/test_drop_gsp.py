71ae7365800c2e738e941657a65087267c8aa486
James Fulton <djamesfulton@yahoo.co.uk>
2023-12-13 15:23:35 +0000

    fix test

--- tests/select/test_drop_gsp.py
+++
@@ -1,6 +0,0 @@
-from ocf_datapipes.select import SelectGSPIDs
-
-
-def test_select_non_national(gsp_datapipe):
-    drop_pipe = SelectGSPIDs(gsp_datapipe)
-    assert 0 not in next(iter(drop_pipe)).gsp_id
