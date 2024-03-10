49dfe54a3831dcd226d6ff8a70a8cb3e6c378428
Markus Kreft <129367085+markus-kreft@users.noreply.github.com>
2024-03-10 14:13:44 +0100

    Add minimal typehints and docstrings

--- ocf_datapipes/batch/utils.py
+++ ocf_datapipes/batch/utils.py
@@ -3,8 +3,17 @@ import numpy as np
 import torch
 
 
-def copy_batch_to_device(batch, device):
-    """Moves a dict-batch of tensors to new device."""
+def copy_batch_to_device(batch: dict, device: torch.device) -> dict:
+    """
+    Moves a dict-batch of tensors to new device.
+
+    Args:
+        batch: dict with tensors to move
+        device: Device to move tensors to
+
+    Returns:
+        A dict with tensors moved to new device
+    """
     batch_copy = {}
 
     for k, v in batch.items():
@@ -18,8 +27,16 @@ def copy_batch_to_device(batch, device):
     return batch_copy
 
 
-def batch_to_tensor(batch):
-    """Moves numpy batch to a tensor"""
+def batch_to_tensor(batch: dict) -> dict:
+    """
+    Moves numpy batch to a tensor
+
+    Args:
+        batch: dict-like batch with data in numpy arrays
+
+    Returns:
+        A batch with data in torch tensors
+    """
     for k, v in batch.items():
         if isinstance(v, dict):
             # Recursion to reach the nested NWP
