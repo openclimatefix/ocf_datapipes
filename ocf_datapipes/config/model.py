d082eb035a871ab7ee61eae4e630ed8fdafda1b4
Sukhil Patel <sukhil@Sukhils-MacBook-Pro.local>
2024-04-03 17:49:40 +0100

    Make system dropout optional

--- ocf_datapipes/config/model.py
+++ ocf_datapipes/config/model.py
@@ -156,7 +156,7 @@ class DropoutMixin(Base):
 class SystemDropoutMixin(Base):
     """Mixin class, to add independent system dropout"""
 
-    system_dropout_timedeltas_minutes: List[int] = Field(
+    system_dropout_timedeltas_minutes: Optional[List[int]] = Field(
         None,
         description="List of possible minutes before t0 where data availability may start. Must be "
         "negative or zero. Each system in a sample is delayed independently from the other by "
