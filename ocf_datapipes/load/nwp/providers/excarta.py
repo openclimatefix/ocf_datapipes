a4f6d31ac29374f84a11d4563c71487f3d470fd4
Jacob Bieker <jacob@bieker.tech>
2024-04-10 14:45:56 +0100

    Update excarta.py

--- ocf_datapipes/load/nwp/providers/excarta.py
+++ ocf_datapipes/load/nwp/providers/excarta.py
@@ -39,6 +39,8 @@ def open_excarta(zarr_path) -> xr.Dataset:
 
     if "hindcast.zarr" in str(zarr_path):  # Preprocessed one
         nwp = open_zarr_paths(zarr_path)
+        nwp = nwp.rename({"__xarray_dataarray_variable__": "excarta"})
+        nwp: xr.DataArray = nwp["excarta"]
         time = pd.DatetimeIndex(nwp.init_time_utc)
         assert time.is_unique
         assert time.is_monotonic_increasing
@@ -70,6 +72,8 @@ def open_excarta(zarr_path) -> xr.Dataset:
         ["10m_wind_speed", "10m_wind_speed_angle", "100m_wind_speed", "100m_wind_speed_angle"]
     )
     nwp: xr.DataArray = nwp.to_array(dim="channel")
+    nwp = nwp.rename({"__xarray_dataarray_variable__": "excarta"})
+    nwp: xr.DataArray = nwp["excarta"]
     # Sanity checks.
     time = pd.DatetimeIndex(nwp.init_time_utc)
     assert time.is_unique
