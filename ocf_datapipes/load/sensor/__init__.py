c5d6030df8e6d84d46a1c04a9326893ccddcc6b2
Jacob Bieker <jacob@bieker.tech>
2024-04-10 14:33:34 +0100

    Fix Excarta loading

--- ocf_datapipes/load/sensor/__init__.py
+++ ocf_datapipes/load/sensor/__init__.py
@@ -1,2 +1,4 @@
 """Loading sensor data (i.e. weather station, rainfall radar, etc.)"""
 from .awos import OpenAWOSFromNetCDFIterDataPipe as OpenAWOSFromNetCDF
+
+# from .meteomatics import OpenMeteomaticsFromNetCDFIterDataPipe as OpenMeteomaticsFromNetCDF
