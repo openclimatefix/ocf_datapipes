f82a479ec481a682befd2eba8eba0eab0a9edc79
Jacob Bieker <jacob@bieker.tech>
2024-04-15 09:29:47 +0100

    Update meteomatics.py

--- ocf_datapipes/load/sensor/meteomatics.py
+++ ocf_datapipes/load/sensor/meteomatics.py
@@ -1,9 +1,11 @@
-"""Load ASOS data from local files for training/inference"""
+"""Load Metoematics data from local files for training/inference"""
 import logging
 
 import fsspec
 import xarray as xr
 from torch.utils.data import IterDataPipe, functional_datapipe
+import numpy as np
+import ocf_blosc2
 
 from ocf_datapipes.config.model import Sensor
 
@@ -31,8 +33,16 @@ class OpenMeteomaticsFromNetCDFIterDataPipe(IterDataPipe):
 
     def __iter__(self):
         with fsspec.open(self.filename, "rb") as f:
-            ds = xr.open_mfdataset(f, engine="zarr", combine="nested", concat_dim="time_utc")
-            ds = ds.sortby("time_utc")
-            ds = ds[self.variables].to_array()
+            data = xr.open_mfdataset(f, engine="zarr", combine="nested", concat_dim="time_utc")
+            data = data.sortby("time_utc")
+            # Generate U and V components from wind speed and direction
+            data["100u"] = data["wind_speed_100m:ms"] * np.cos(np.deg2rad(data["wind_dir_100m:d"]))
+            data["100v"] = data["wind_speed_100m:ms"] * np.sin(np.deg2rad(data["wind_dir_100m:d"]))
+            data["10u"] = data["wind_speed_10m:ms"] * np.cos(np.deg2rad(data["wind_dir_10m:d"]))
+            data["10v"] = data["wind_speed_10m:ms"] * np.sin(np.deg2rad(data["wind_dir_10m:d"]))
+            data["200u"] = data["wind_speed_200m:ms"] * np.cos(np.deg2rad(data["wind_dir_200m:d"]))
+            data["200v"] = data["wind_speed_200m:ms"] * np.sin(np.deg2rad(data["wind_dir_200m:d"]))
+            data = data[self.variables].to_array()
         while True:
-            yield ds
+            yield data
+
