08350f25b443359891b2ab973c2ffdb1367a855e
James Fulton <djamesfulton@yahoo.co.uk>
2024-04-15 19:01:29 +0000

    standardize pick functions

--- ocf_datapipes/select/pick_locations.py
+++ ocf_datapipes/select/pick_locations.py
@@ -17,51 +17,79 @@ class PickLocationsIterDataPipe(IterDataPipe):
     def __init__(
         self,
         source_datapipe: IterDataPipe,
-        return_all_locations: bool = False,
+        return_all: bool = False,
+        shuffle: bool = False,
     ):
         """
-        Picks random locations from a dataset
+        Datapipe to yield locations from the input data source.
 
         Args:
             source_datapipe: Datapipe emitting Xarray Dataset
-            return_all_locations: Whether to return all locations,
-                if True, also returns them in order
+            return_all: Whether to return all t0-location pairs,
+                if True, also returns them in structured order
+            shuffle: If `return_all` is True this sets whether the pairs are
+                shuffled before being returned.
         """
         super().__init__()
         self.source_datapipe = source_datapipe
-        self.return_all_locations = return_all_locations
+        self.return_all = return_all
+        self.shuffle = shuffle
+
+    def _yield_all_iter(self, xr_dataset):
+        
+        # Get the spatial coords
+        xr_coord_system, xr_x_dim, xr_y_dim = spatial_coord_type(xr_dataset)
+        
+        loc_indices = np.arange(len(xr_dataset[xr_x_dim]))
+
+        if self.shuffle:
+            loc_indices = np.random.permutation(loc_indices)
+
+        # Iterate through all locations in dataset
+        for loc_index in loc_indices:
+            
+            # Get the location ID
+            loc_id = None
+            for id_dim_name in ["pv_system_id", "gsp_id", "station_id"]:
+                if id_dim_name in xr_dataset.coords.keys():
+                    loc_id = int(xr_dataset[id_dim_name][loc_index].values)
+            
+            location = Location(
+                coordinate_system=xr_coord_system,
+                x=xr_dataset[xr_x_dim][loc_index].values,
+                y=xr_dataset[xr_y_dim][loc_index].values,
+                id=loc_id,
+            )
+
+            yield location
+
+    def _yield_random_iter(self, xr_dataset):
+        
+        # Get the spatial coords
+        xr_coord_system, xr_x_dim, xr_y_dim = spatial_coord_type(xr_dataset)
+
+        while True:
+            loc_index = np.random.randint(0, len(xr_dataset[xr_x_dim]))
+            
+            # Get the location ID
+            loc_id = None
+            for id_dim_name in ["pv_system_id", "gsp_id", "station_id"]:
+                if id_dim_name in xr_dataset.coords.keys():
+                    loc_id = int(xr_dataset[id_dim_name][loc_index].values)
+
+            location = Location(
+                coordinate_system=xr_coord_system,
+                x=xr_dataset[xr_x_dim][loc_index].values,
+                y=xr_dataset[xr_y_dim][loc_index].values,
+                id=loc_id,
+            )
+            
+            yield location
 
     def __iter__(self) -> Location:
-        """Returns locations from the inputs datapipe"""
-        for xr_dataset in self.source_datapipe:
-            loc_type, xr_x_dim, xr_y_dim = spatial_coord_type(xr_dataset)
+        xr_dataset = next(iter(self.source_datapipe))
 
-            if self.return_all_locations:
-                logger.debug("Going to return all locations")
-
-                # Iterate through all locations in dataset
-                for location_idx in range(len(xr_dataset[xr_x_dim])):
-                    location = Location(
-                        x=xr_dataset[xr_x_dim][location_idx].values,
-                        y=xr_dataset[xr_y_dim][location_idx].values,
-                        coordinate_system=loc_type,
-                    )
-                    if "pv_system_id" in xr_dataset.coords.keys():
-                        location.id = int(xr_dataset["pv_system_id"][location_idx].values)
-                    logger.debug(f"Got all location {location}")
-                    yield location
-            else:
-                # Pick 1 random location from the input dataset
-                logger.debug("Selecting random idx")
-                location_idx = np.random.randint(0, len(xr_dataset[xr_x_dim]))
-                logger.debug(f"{location_idx=}")
-                location = Location(
-                    x=xr_dataset[xr_x_dim][location_idx].values,
-                    y=xr_dataset[xr_y_dim][location_idx].values,
-                    coordinate_system=loc_type,
-                )
-                if "pv_system_id" in xr_dataset.coords.keys():
-                    location.id = int(xr_dataset["pv_system_id"][location_idx].values)
-                    logger.debug(f"Have selected location.id {location.id}")
-                logger.debug(f"{location=}")
-                yield location
+        if self.return_all:
+            return self._yield_all_iter(xr_dataset)
+        else:
+            return self._yield_random_iter(xr_dataset)
