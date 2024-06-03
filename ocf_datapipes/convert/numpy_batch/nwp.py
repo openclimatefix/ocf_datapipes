5d1551b0ec2b7107cd1864a83b08ccde06781541
James Fulton <djamesfulton@yahoo.co.uk>
2024-06-03 14:43:34 +0000

    fix

--- ocf_datapipes/convert/numpy_batch/nwp.py
+++ ocf_datapipes/convert/numpy_batch/nwp.py
@@ -13,20 +13,19 @@ def convert_nwp_to_numpy_batch(xr_data):
         NWPBatchKey.nwp: xr_data.values,
         NWPBatchKey.nwp_t0_idx: xr_data.attrs["t0_idx"],
         NWPBatchKey.nwp_channel_names: xr_data.channel.values,
-        NWPBatchKey.nwp_step: (xr_data.step.values / np.timedelta64(1, "h")).astype(np.int64),
         NWPBatchKey.nwp_init_time_utc: datetime64_to_float(xr_data.init_time_utc.values),
+        NWPBatchKey.nwp_step: (xr_data.step.values / np.timedelta64(1, "h")).astype(np.int64),
     }
-
-    if "target_time_utc" in xr_data:
-        example[NWPBatchKey.nwp_target_time_utc] = datetime64_to_float(
-            xr_data.target_time_utc.values
-        )
-
+    
+    if "target_time_utc" in xr_data.coords:
+        target_time = xr_data.target_time_utc.values
+        example[NWPBatchKey.nwp_target_time_utc] = datetime64_to_float(target_time)
+        
     for batch_key, dataset_key in (
         (NWPBatchKey.nwp_y_osgb, "y_osgb"),
         (NWPBatchKey.nwp_x_osgb, "x_osgb"),
     ):
-        if dataset_key in xr_data:
+        if dataset_key in xr_data.coords:
             example[batch_key] = xr_data[dataset_key].values
 
     return example
