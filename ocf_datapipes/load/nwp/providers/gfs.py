4f282189470f2927d3c88e83fd6212796c58b9ff
Alexandra Udaltsova <43303448+AUdaltsova@users.noreply.github.com>
2024-06-28 15:54:07 +0100

    remove extrapolation option gfs.py

--- ocf_datapipes/load/nwp/providers/gfs.py
+++ ocf_datapipes/load/nwp/providers/gfs.py
@@ -6,8 +6,6 @@ import xarray as xr
 
 from ocf_datapipes.load.nwp.providers.utils import open_zarr_paths
 
-# import pandas as pd
-
 _log = logging.getLogger(__name__)
 
 
@@ -25,21 +23,6 @@ def open_gfs(zarr_path) -> xr.DataArray:
 
     # Open data
     gfs: xr.Dataset = open_zarr_paths(zarr_path, time_dim="init_time_utc")
-
-    # _________________EXTRAPOLATION_________________
-
-    # _log.info("Imputing step 0 for radiation variables")
-    #
-    # flux_vars = ['dswrf', 'dlwrf']
-    # for var in flux_vars:
-    #     gfs[var] = xr.concat([
-    #         gfs[var].sel(step=slice(pd.Timedelta(hours=3), None)).interp(
-    #             step=pd.Timedelta(hours=0),
-    #             kwargs={"fill_value": "extrapolate"}
-    #         ),
-    #         gfs[var].sel(step=slice(pd.Timedelta(hours=3), None))
-    #     ], dim='step')
-
     nwp: xr.DataArray = gfs.to_array()
 
     del gfs
