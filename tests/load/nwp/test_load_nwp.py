32bfe1e564f26ef7d132a54960935231acac20c5
Alexandra Udaltsova <43303448+AUdaltsova@users.noreply.github.com>
2024-06-28 11:25:44 +0100

    Update gfs test data path test_load_nwp.py

--- tests/load/nwp/test_load_nwp.py
+++ tests/load/nwp/test_load_nwp.py
@@ -106,7 +106,7 @@ def test_load_excarta_local():
 
 def test_load_gfs():
     nwp_datapipe = OpenNWP(
-        zarr_path="tests/data/gfs.zarr.zip",
+        zarr_path="tests/data/gfs.zarr",
         provider="merra2",
     )
     metadata = next(iter(nwp_datapipe))
